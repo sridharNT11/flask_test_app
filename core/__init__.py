5f17f87faa8665a43601a33f4fbd061eaa3b4b79
Sridharan.R <sridharan.r@SridharanRs-MacBook-Air.local>
2020-09-04 15:24:19 +0530

    v.2

--- core/__init__.py
+++ core/__init__.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 
 app.config.from_object('core.config.SECRET_KEY')
-app.config.from_object('core.config.ProductionConfig')
+app.config.from_object('core.config.Config')
 
 
 config = app.config
--- core/config.py
+++ core/config.py
@@ -4,7 +4,7 @@ class Config(object):
     DATABASE_URI = 'sqlite:///:memory:'
 
 class ProductionConfig(Config):
-    DATABASE_URI = 'mysql+mysqldb://root:@127.0.0.1:3306/test_app'
+    DATABASE_URI = 'mysql+mysqldb://urbanedg_train:P&,1(r7-+o2o@72.52.161.232:3306/urbanedg_training_project'
     CACHE_TYPE  = 'NULL'
     
 class DevelopmentConfig(Config):
