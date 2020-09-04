0139750661c8ad8c5e0c1594c50110a8af1b2cc3
Sridharan.R <sridharan.r@SridharanRs-MacBook-Air.local>
2020-09-04 15:41:19 +0530

    v.2

--- core/config.py
+++ core/config.py
@@ -2,7 +2,7 @@ class Config(object):
     DEBUG = False
     TESTING = False
     DATABASE_URI = 'mysql+mysqldb://urbanedg_train:P&,1(r7-+o2o@72.52.161.232:3306/urbanedg_training_project'
-    ENV = ""
+    ENV = "development"
 
 class ProductionConfig(Config):
     DATABASE_URI = 'mysql+mysqldb://root:@127.0.0.1:3306/test_app'
